152a7be18c39081ac534365ecbc04dcd6303e285
Mahesh Kulkarni <mails.mahesh@gmail.com>
2017-02-02 12:55:42 -0800

    Update webpay.js

--- src/js/webpay.js
+++ src/js/webpay.js
@@ -25,7 +25,8 @@ webpay.prototype.setup = function(itemSummary, total){
  			'merchantName': 'Shop Samsung (demo)',		
  			'debug': {		
  				'APIKey': '6874ad7c7c10403396811780aef9ecf3'		
- 		}		
+ 			}		
+		}
  	}];
 
 	var details = {
