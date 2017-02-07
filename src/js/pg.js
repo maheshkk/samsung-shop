82b3258576250500072e3274111ede177977f18d
Mahesh Kulkarni <mahesh.kk@samsung.com>
2017-02-06 16:00:18 -0800

    missing file

---
+++ src/js/pg.js
@@ -0,0 +1,51 @@
+function guid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {var r = Math.random()*16|0,v=c=='x'?r:r&0x3|0x8;return v.toString(16);});
+}
+
+function processPayment(payload) {
+    return new Promise(function (resolve, reject) {    
+        if (!payload || !payload.paymentCredential) {
+            resolve(false);
+        }
+
+        var credentials = JSON.parse(payload.paymentCredential)["3ds"];
+
+        var postPayment = { 
+            "request_id": guid(),
+            "mid": "2cae108f-c342-4a79-b8f9-bb524112ab17",
+            "txn_type": "PURCHASE",
+            "method": "3ds",
+            "currency": "USD",
+            "amount": 500,
+            "3ds" : credentials
+        }
+
+        fetch('https://api.samsungpaydev.us/papi/v1/transactions', {
+            method: 'post',
+            body: JSON.stringify(postPayment),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then(function(response) {
+            if (!response.ok) {
+                //handle error
+                resolve(false);
+            }
+            return response.json();
+        }).then(function(paymentVerified) {
+            try { 
+                if (paymentVerified && paymentVerified.resp_code && paymentVerified.resp_code == "APPROVAL") {
+                    console.log("RES: " + paymentVerified);
+                    resolve(true);
+                }
+            } catch(e) {
+                console.log("unexpected response")
+                reject(false);
+            }
+        }).catch(function(err) {
+            console.log("Err: " + err.message);
+            reject(false);            
+        });
+    });        
+}
