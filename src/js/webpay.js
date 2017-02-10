d5371f89087aff8f0010ce96ad48f3d0c0d2afef
Winston Chen <winston.c1@samsung.com>
2017-02-10 17:51:37 -0500

    making sure total is calculated correctly

--- src/js/webpay.js
+++ src/js/webpay.js
@@ -109,10 +109,12 @@ webpay.prototype.setup = function(itemSummary, total){
 	      selectedShippingOption = details.shippingOptions[0];
 	      otherShippingOption = details.shippingOptions[1];
 	      details.total.amount.value -= 10.00;
+	      console.log(details.total.amount.value);
 	    } else {
 	      selectedShippingOption = details.shippingOptions[1];
 	      otherShippingOption = details.shippingOptions[0];
 	      details.total.amount.value -= 25.00;
+	      console.log(details.total.amount.value);
 	    }
 	    if (details.displayItems.length === 2) {
 	      details.displayItems.splice(1, 0, selectedShippingOption);
