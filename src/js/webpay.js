951bb74603812e119837f40f4cc1fb72aeb6db66
Winston Chen <winston.c1@samsung.com>
2017-02-10 17:08:52 -0500

    adding shipping option change event listener

--- src/js/webpay.js
+++ src/js/webpay.js
@@ -99,6 +99,15 @@ webpay.prototype.setup = function(itemSummary, total){
 		}));
 	});
 	 
+ 	//detect shipping option changes
+ 	payment.addEventListener('shippingoptionchange', function(e) {
+ 		console.log('shipping option changed');
+	  e.updateWith( new Promise( function(details, shippingOption) {
+	    resolve(details);
+	  }));
+	});
+
+
 	// Make PaymentRequest show to display payment sheet 
 	payment.show().then(function(paymentResponse) {	
 	  // Process response
