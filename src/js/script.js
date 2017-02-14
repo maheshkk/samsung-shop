59173e5949ae8e99131eb8f0b1c1f512c8df4d3b
007 <winstonc@bu.edu>
2017-02-13 19:42:41 -0500

    adding a test ppost method

--- src/js/script.js
+++ src/js/script.js
@@ -36,13 +36,26 @@ $().ready(function() {
 				"amount": 1700, 
 				"3ds": { "type": "S", "version": "100", "data": "eyJhbGciOiJSU0ExXzUiLCJraWQiOiIyYUpFSUIrZ3Z2MmxEZFA4ZnczNUNvWW1uS0VWeC9UZUV5ODNXT0UxQkRjPSIsInR5cCI6IkpPU0UiLCJjaGFubmVsU2VjdXJpdHlDb250ZXh0IjoiUlNBX1BLSSIsImVuYyI6IkExMjhHQ00ifQ.ghEZgr6KZFa2AJpWOr9Q7Wb3nkHdtHfEUkzzsEeuH_RUBUXIHm363Gye4iqQ8ffZBN19_TAfn4AVLbB7gGLKRIigmjhwAVeU54QZNAmd6w10bK2JuX0FQDPfe0SDKVBi5EGgJE9vfmUDnRNnS_7_s3P9yIn1IclcD7TYUyw4zJPFubE872oaw-cGM-YFHxRceBU3RbaAmWWfWpOiA54KRqofxC9ZC-mWPGUytvB3fxxVH-cw_KiTYYaXUqrM6czmAZWLqg7K7DPeBAJKVWL3KOwq4Bdd4CSj7FXM_0RLOtdH-qFNG8xR8JfPuPjb2x5UY-U8HEIuIRlCIz9FTZcx6g.oMNQMFnRiCQyVDJB.sqPFdYcUjvzsxsa0AhutJgq17pGdQshL2v9ncvxQCjxGxu7LWbvOfnW9HsxTHXe3VirQpvNOlf4QEb46_jjOdwWygmvleg_7bEstf9fqZFgqeg3aC03qk0_jE4DIMNz9Tw-FWCk1xrzee4zLNa8mUJKyEZ4wVI6YIGnrcBbEJNS691O47VUwsnuH41uU9Sniloc-ckgXo0vIZKHxAp_n1FAcRaakzZPigAfkxMh6WGm9O0McyNGnMA.Q7BBY9FY2peHwo2lKSMpxA" }
 			};
-		    $.ajax({
-		  	url: 'https://api.samsungpaydev.us/pcat/v1/transactions',
-		  	data: info,
-		  	success: function(response){
-		  		console.log(response);
-		  	}
-		});
+		  	var url = 'https://api.samsungpaydev.us/pcat/v1/transactions',
+		  	
+		    fetch(url)  
+			  .then(  
+			    function(response) {  
+			      if (response.status !== 200) {  
+			        console.log('Looks like there was a problem. Status Code: ' +  
+			          response.status);  
+			        return;  
+			      }
+
+			      // Examine the text in the response  
+			      response.json().then(function(data) {  
+			        console.log(data);  
+			      });  
+			    }  
+			  )  
+			  .catch(function(err) {  
+			    console.log('Fetch Error :-S', err);  
+			  });
 			var itemSummary = [{
 				'label': prod_name,
 				'value': prod_price
