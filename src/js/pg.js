ba0d81ce185553bb1f0b37d7389514f609608354
007 <winstonc@bu.edu>
2017-02-21 20:13:17 -0500

    moved creds to pg.js

--- src/js/pg.js
+++ src/js/pg.js
@@ -7,8 +7,18 @@ function guid() {
 function processPayment(payload, totalCost) {
     console.log(payload);
     return new Promise(function (resolve, reject) {    
-        if (!payload || !payload.details || !payload.details.paymentCredential) {
+        if (!payload || !payload.details) {
            resolve(false);
+        }
+        var credentials;
+        if(!payload.details.paymentCredential){
+            credentials = {
+                "type":"S",
+                "version":"100",
+                "data":"eyJhbGciOiJSU0ExXzUiLCJraWQiOiIyYUpFSUIrZ3Z2MmxEZFA4ZnczNUNvWW1uS0VWeC9UZUV5ODNXT0UxQkRjPSIsInR5cCI6IkpPU0UiLCJjaGFubmVsU2VjdXJpdHlDb250ZXh0IjoiUlNBX1BLSSIsImVuYyI6IkExMjhHQ00ifQ.ghEZgr6KZFa2AJpWOr9Q7Wb3nkHdtHfEUkzzsEeuH_RUBUXIHm363Gye4iqQ8ffZBN19_TAfn4AVLbB7gGLKRIigmjhwAVeU54QZNAmd6w10bK2JuX0FQDPfe0SDKVBi5EGgJE9vfmUDnRNnS_7_s3P9yIn1IclcD7TYUyw4zJPFubE872oaw-cGM-YFHxRceBU3RbaAmWWfWpOiA54KRqofxC9ZC-mWPGUytvB3fxxVH-cw_KiTYYaXUqrM6czmAZWLqg7K7DPeBAJKVWL3KOwq4Bdd4CSj7FXM_0RLOtdH-qFNG8xR8JfPuPjb2x5UY-U8HEIuIRlCIz9FTZcx6g.oMNQMFnRiCQyVDJB.sqPFdYcUjvzsxsa0AhutJgq17pGdQshL2v9ncvxQCjxGxu7LWbvOfnW9HsxTHXe3VirQpvNOlf4QEb46_jjOdwWygmvleg_7bEstf9fqZFgqeg3aC03qk0_jE4DIMNz9Tw-FWCk1xrzee4zLNa8mUJKyEZ4wVI6YIGnrcBbEJNS691O47VUwsnuH41uU9Sniloc-ckgXo0vIZKHxAp_n1FAcRaakzZPigAfkxMh6WGm9O0McyNGnMA.Q7BBY9FY2peHwo2lKSMpxA"
+            }
+        } else {
+            credentials = payload.details.paymentCredential["3DS"];
         }
         
         var server = {};
@@ -22,11 +32,9 @@ function processPayment(payload, totalCost) {
             server['url'] = '/pcat/v1/transactions';
         } else if(serverSwitch === 'stripe') {
             server['mid'] = '18cb7fde-5321-4dba-b9ee-13a49e172f7c';
-	    server['url'] = '/papi/v1/transactions';
+	        server['url'] = '/papi/v1/transactions';
         } else {
-	}
-        
-        var credentials = payload.details.paymentCredential["3DS"];
+	    }
         var url = 'https://api.samsungpaydev.us' + server['url'];
         console.log(url);
 
