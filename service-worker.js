a3f61eecc9a0ae8a5e6c3cc713dfe0026684a992
Winston Chen <winston.c1@samsung.com>
2017-03-15 17:23:23 -0400

    debugging pwa

--- service-worker.js
+++ service-worker.js
@@ -5,35 +5,40 @@
 // Use a cacheName for cache versioning
 var cacheName = 'samsung-shop-pwa';
 
+var filesToCache = [
+    './',
+    './src/css/styles.css',
+    './src/js/cart.js',
+    './src/js/pg.js',
+    './src/js/script.js',
+    './src/js/webpay.js',
+    './cart.html',
+    './checkout.html',
+    './index.html',
+    './order-confirm.html',
+    './src/img/logo.png',
+    './src/img/samsunggalaxys7.jpg',
+    './src/img/gears3frontier.jpg',
+    './src/img/gears3classic.jpg',
+    './src/img/gear360camera.jpg',
+    './src/img/gearvr2.jpg',
+    './src/img/gearvr.jpg',
+    './src/img/wirelesscharger.jpg',
+    './src/img/gears2.jpg',
+    './src/img/s3wirelesscharger.jpg',
+    './src/img/bluetoothheadphones.jpg',
+    './src/img/oledtv.jpg'
+    ];
+
 // During the installation phase, you'll usually want to cache static assets.
 self.addEventListener('install', e => {
-    // Once the service worker is installed, go ahead and fetch the resources to make this work offline.
+    console.log('[ServiceWorker] Install');
+    // Once the service worker is installed, go ahead and 
+    // fetch the resources to make this work offline.
     e.waitUntil(
         caches.open(cacheName).then( cache => {
-            return cache.addAll([
-                './',
-                './src/css/styles.css',
-                './src/js/cart.js',
-                './src/js/pg.js',
-                './src/js/script.js',
-                './src/js/webpay.js',
-                './cart.html',
-                './checkout.html',
-                './index.html',
-                './order-confirm.html',
-                './src/img/logo.png',
-                './src/img/samsunggalaxys7.jpg',
-                './src/img/gears3frontier.jpg',
-                './src/img/gears3classic.jpg',
-                './src/img/gear360camera.jpg',
-                './src/img/gearvr2.jpg',
-                './src/img/gearvr.jpg',
-                './src/img/wirelesscharger.jpg',
-                './src/img/gears2.jpg',
-                './src/img/s3wirelesscharger.jpg',
-                './src/img/bluetoothheadphones.jpg',
-                './src/img/oledtv.jp'
-            ]).then(function() {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache).then(function() {
                 self.skipWaiting();
             });
         })
